25266979f0f157db49067ad6922a50da4ae64848
svorfolomeev <vorfol@yandex.ru>
2020-07-02 14:37:52 +0500

    pwd

--- Lib/test/test_pwd.py
+++ Lib/test/test_pwd.py
@@ -4,6 +4,7 @@ from test import support
 
 pwd = support.import_module('pwd')
 
+@unittest.skipIf(sys.platform == 'OpenVMS', 'OpenVMS: the process must have appropriate privileges')
 @unittest.skipUnless(hasattr(pwd, 'getpwall'), 'Does not have getpwall()')
 class PwdTest(unittest.TestCase):
 
